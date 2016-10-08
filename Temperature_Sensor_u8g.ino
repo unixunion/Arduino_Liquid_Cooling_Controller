ad4278f972ab34fb682bd92a72f4cb09bd6cd6ae
UnixUnion <marzubus@gmail.com>
2016-10-08 12:11:06 +0200

    logo

--- Temperature_Sensor_u8g.ino
+++ Temperature_Sensor_u8g.ino
@@ -104,7 +104,7 @@ void setup(void)
   do {
 //    display.drawXBMP( 0, 0, logo_width, logo_height, logo_bits);
 //    display.setColorIndex(1);
-  display.drawStr(0,0,"deBlox");
+  display.drawStr(0,0,"Psimax");
   display.drawStr(32,24,"Systems");
   display.setColorIndex(1);
   } while( display.nextPage() );
